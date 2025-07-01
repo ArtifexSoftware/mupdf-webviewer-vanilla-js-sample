f52b1fa63456b27c53256d7e97abe80aa40398b2
Jamie Lemon <jamie.lemon@artifex.com>
2025-07-01 23:37:29 +0100

    Updates project to show how to open remote PDFs as blob URLs.

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-import { initMuPDFWebViewer } from '/node_modules/mupdf-webviewer/index.js';
+import { initMuPDFWebViewer } from './node_modules/mupdf-webviewer/index.js';
 
-try {
-  const mupdf = await initMuPDFWebViewer(
-    '#viewer',
-    '/sample.pdf',
-    {
-      filename: 'sample.pdf',
-      libraryPath: '/lib',
-      licenseKey: 'YOUR_LICENSE_KEY',
-    }
-  )
-
-} catch (error) {
-  console.error('init error');
-  console.error(error);
+async function openRemotePDF(url, filename = 'file.pdf') {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  const blobUrl = URL.createObjectURL(blob);
+  openPDF(blobUrl, filename);
 }
+
+async function openPDF(url, filename) {
+    try {
+        const mupdf = await initMuPDFWebViewer(
+            '#viewer',
+            url,
+            {
+                filename: filename,
+                libraryPath: 'lib',
+                licenseKey: 'YOUR_LICENSE_KEY', // if your project is in a sub-folder from root then you need to target this e.g. '/webviewer/lib'
+            }
+        )
+    } catch (error) {
+        console.error('init error');
+        console.error(error);
+    }
+}
+
+
+
+// open a local PDF
+openPDF('/sample.pdf', 'sample.pdf')
+
+// or open a remote PDF
+// ensure CORS access control origin for the PDF file is permitted
+// openRemotePDF('https://dijipiji.com/sample.pdf', 'sample.pdf');
